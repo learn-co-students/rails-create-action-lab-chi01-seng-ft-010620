c6480df8d092f9bd1a2ff4113b34e9a2456f77cb
Cesar <cesarmota@ymail.com>
2020-02-03 15:41:37 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,9 @@ Rails.application.routes.draw do
   resources :students, only: [:index, :new, :create]
   get '/student/:id', to: 'students#show', as: 'student'
 end
+
+#     Prefix  Verb  URI Pattern           Controller#Action
+#    students GET  /students(.:format)     students#index
+#             POST /students(.:format)     students#create
+# new_student GET  /students/new(.:format) students#new
+#     student GET  /student/:id(.:format)  students#show
